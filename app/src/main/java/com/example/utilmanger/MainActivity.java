0f67bab3d02c99b6f2245fe29f181c7a3819593b
zhaolichong <zhaolichong@haiwainet.cn>
2020-10-22 16:13:31 +0800

    动画json

--- app/src/main/java/com/example/utilmanger/MainActivity.java
+++ app/src/main/java/com/example/utilmanger/MainActivity.java
@@ -4,20 +4,38 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.airbnb.lottie.LottieAnimationView;
 import com.example.utilmanger.manger.LayoutManger;
 
 public class MainActivity extends AppCompatActivity {
+
+    private LottieAnimationView lottie_view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        /**
+         * 约束布局动态设置
+         */
         ConstraintLayout constraintlayout = (ConstraintLayout) findViewById(R.id.constraint);
-        TextView tv_constraint = (TextView) findViewById(R.id.tv_constraint);
-        EditText et_edit = (EditText) findViewById(R.id.et_edit);
+        TextView tv_constraint = findViewById(R.id.tv_constraint);
+        EditText et_edit = findViewById(R.id.et_edit);
         LayoutManger.setConstraintLayout(constraintlayout, tv_constraint, et_edit, this);
+        /**
+         * 动画json
+         */
+        lottie_view = findViewById(R.id.lottie_view);
+        lottie_view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                lottie_view.playAnimation();
+            }
+        });
+
     }
 }
